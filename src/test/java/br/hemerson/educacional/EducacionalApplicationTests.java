2598b222fd65c7d534e2649359b4d3bbc8d65df8
Hemex001 <83310574+Hemex001@users.noreply.github.com>
2024-11-25 12:57:44 -0300

    start repo

---
+++ src/test/java/br/hemerson/educacional/EducacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.hemerson.educacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
